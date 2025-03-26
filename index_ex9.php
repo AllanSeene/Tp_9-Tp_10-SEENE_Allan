d5e13493136bcbbdd00eb2547633c2d28e49ae80
Allan <allan.seene@outlook.fr>
2025-03-26 11:51:22 +0100

    Fin_partie_2

--- index_ex9.php
+++ index_ex9.php
@@ -4,4 +4,39 @@
 
 ?>
 
+<p> <strong>Prenom et nom des étudiants :</strong></p>
+    <?php
+        $sql = "SELECT prenom, nom FROM etudiants;";
+        $result = $dbPDO->query($sql);
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+        while ($row = $result->fetch()) {
+            echo $row['prenom'];
+            echo " ";
+            echo $row['nom'];
+            echo "<br>";
+        }
+    ?>
 
+<p> <strong>Liste des classes </strong></p>
+    <?php
+        $sql = "SELECT libelle FROM classes;";
+        $result = $dbPDO->query($sql);
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+        while ($row = $result->fetch()) {
+            echo $row['libelle'];
+            echo "<br>";
+        }
+    ?>
+
+<p> <strong>Prenom et nom des professeurs :</strong></p>
+    <?php
+        $sql = "SELECT prenom, nom FROM professeurs;";
+        $result = $dbPDO->query($sql);
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+        while ($row = $result->fetch()) {
+            echo $row['prenom'];
+            echo " ";
+            echo $row['nom'];
+            echo "<br>";
+        }
+    ?>
